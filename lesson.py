be8f6664fee36b8e28e911c3c4b67e9f0356d3de
Nika <129578493+nixiwqq@users.noreply.github.com>
2024-10-04 20:30:47 +0200

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,18 @@
+#task 1
+#x = int(input('Enter a number:'))
+#y = int(input('Enter another number:'))
+#print('додавання:', x + y)
+#print('віднімання:', x - y)
+#print('множення:', x * y)
+
+#task 2
+#value = int(input('Enter value:'))
+#percent = int(input('Enter percent:'))
+#print('let\'s find a percent of your value')
+#print((percent / value) * 100)
+
+#task 3
+#x = int(input('Enter a height:'))
+#y = int(input('Enter another width:'))
+#print("Знаходимо площу прямокутника:")
+#print(x*y)
